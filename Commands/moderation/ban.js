97e4392a52351687d021cb27392b317eb00ecccc
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:15:25 -0500

    Update ban.js

--- Commands/moderation/ban.js
+++ Commands/moderation/ban.js
@@ -1,8 +1,15 @@
 modules.export = {
   guildOnly: true,
-  name: "",
+  name: "ban",
   
   execute(message, args) {
-    //
+    if(message.member.hasPermission("BAN_MEMBERS")) {
+      let member = message.mentions.members.first();
+      if(!member) return message.reply("Please mention a valid member of this server");
+      if(!member.bannable) return message.reply("I cannot ban this member!");
+      const banReason = args.slice(1).join(' ');
+
+      member.ban({reason: banReason});
+    }
   }
 }
