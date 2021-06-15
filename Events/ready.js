d551b8ab2232fcca4061ac6dd4bf0a493fa5ed02
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:07:25 -0500

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,1 +1,12 @@
-
+module.exports = {
+  name: "ready",
+  once: true,
+  
+  execute(Client) {
+    console.log(`USER: ${Client.user.tag}`);
+    console.log(`ID: Client.user.id`);
+    console.log("------------------------");
+    
+    Client.user.setActivity(`in ${Client.guilds.cache.size} server(s).`);
+  }
+}
