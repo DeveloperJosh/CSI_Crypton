91009305d59c90c57aaf9ddc9a5443a2d0082e3e
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:17:19 -0500

    Update softban.js

--- Commands/moderation/softban.js
+++ Commands/moderation/softban.js
@@ -1,8 +1,13 @@
 modules.export = {
   guildOnly: true,
-  name: "",
+  name: "sban",
   
   execute(message, args) {
-    //
+    let member = message.mentions.members.first();
+    if(!member) return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) return message.reply("I cannot ban this member!");
+    const banReason = args.slice(1).join(' ');
+    
+    member.ban({days: 7, reason: banReason});
   }
 }
