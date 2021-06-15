43d1c205da21d6e73fb846294aa9b1b92cd7be79
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:37:32 -0500

    Update unban.js

--- Commands/utilities/unban.js
+++ Commands/utilities/unban.js
@@ -4,12 +4,13 @@ module.export = {
   
   execute(message, args) {
     if(message.member.hasPermission("BAN_MEMBERS")) {
-      let userID = args[0]
-      message.guild.fetchBans().then(bans=> {
-      if(bans.size == 0) return 
-      let bUser = bans.find(b => b.user.id == userID)
-      if(!bUser) return
-      message.guild.members.unban(bUser.user)
+        let userID = args[0]
+        message.guild.fetchBans().then(bans=> {
+        if(bans.size == 0) return 
+        let bUser = bans.find(b => b.user.id == userID)
+        if(!bUser) return
+        message.guild.members.unban(bUser.user)
+      }
     }
   }
 }
