9d2e4c24f78de0a3f1f64da1ddf7fb03861adaa5
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:57:04 -0500

    Update guildPrefix.js

--- models/guildPrefix.js
+++ models/guildPrefix.js
@@ -1,1 +1,10 @@
+const Mongoose = require('mongoose');
 
+const PrefixSchema = new Mongoose.Schema({
+    Prefix: {
+        type: String
+    },
+    GuildID: String
+});
+
+const MessageModel = module.exports = Mongoose.model('prefixes', PrefixSchema);
