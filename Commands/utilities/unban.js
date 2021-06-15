6e01fc180fd180d667de41f802daf6035e10c4e3
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:21:27 -0500

    Update unban.js

--- Commands/utilities/unban.js
+++ Commands/utilities/unban.js
@@ -3,6 +3,13 @@ modules.export = {
   name: "unban",
   
   execute(message, args) {
-    //
+    if(message.member.hasPermission("BAN_MEMBERS")) {
+      let userID = args[0]
+      message.guild.fetchBans().then(bans=> {
+      if(bans.size == 0) return 
+      let bUser = bans.find(b => b.user.id == userID)
+      if(!bUser) return
+      message.guild.members.unban(bUser.user)
+    }
   }
 }
