89997d8a079dbc110a03eaaab0d80ee8ccf7de44
Mason Lough <70122910+Synterragen@users.noreply.github.com>
2021-06-14 19:19:38 -0500

    Update kick.js

--- Commands/moderation/kick.js
+++ Commands/moderation/kick.js
@@ -1,8 +1,14 @@
 modules.export = {
   guildOnly: true,
-  name: "",
+  name: "kick",
   
   execute(message, args) {
-    //
+    if(message.member.hasPermission("KICK_MEMBERS")) {
+      let member = message.mentions.members.first();
+      if(!member) return message.reply("Please mention a valid member of this server");
+      if(!member.kickable) return message.reply("I cannot ban this member!");
+
+      member.kick();
+    }
   }
 }
